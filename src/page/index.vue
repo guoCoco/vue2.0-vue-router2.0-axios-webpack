f0e344933f67dbb4d0e21114a282ea751c5d0c9c
guokk <guokk@tsfa.com.cn>
2018-04-10 15:23:05 +0800

    基于webpack3+vue2.0+axios+vue-router2.0项目搭建

---
+++ src/page/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id='main'>
+    <Header/>
+    <div id="cont">ssss</div>
+    <ul>
+      <li v-for='(val, i) in list' v-bind:key='i'>
+        <time v-text='val.create_at'></time>
+        <router-link :to="'/comtent/' + val.id">
+          {{val.title}}
+        </router-link>
+      </li>
+    </ul>
+    <Footer/>
+  </div>
+
+</template>
+<script>
+import Header from '../components/header.vue'
+import Footer from '../components/footer.vue'
+export default {
+  components: {Header, Footer},
+  data () {
+    return {
+      list: []
+    }
+  },
+  created () {
+    // var data = {
+    //   topid: '7',
+    //   showapi_sign: '118146df7242463290977c32d281bf6f',
+    //   showapi_appid: '60810'
+    // }
+    // this.$http.post('/213-4', data, (res) => {
+    //   console.log(res)
+    // })
+    this.$http.get('/topics', null, (res) => {
+      console.log(res)
+      this.list = res.data
+    })
+  },
+  mounted () {
+    let date = new Date()
+    console.log(this.$utils.formatDate(date))
+  }
+}
+</script>
+<style lang='scss'>
+  #main{
+    #cont{
+      font-size: 32px;
+      color: red;
+    }
+  }
+</style>
---
+++ src/utils/index.js
@@ -0,0 +1,20 @@
+// 常用工具文件,一些公共函数的处理
+const formatNumber = (num) => {
+  num = num.toString()
+  return num[1] ? num : 0 + num
+}
+
+const formatDate = (date) => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+module.exports = {
+  formatDate: formatDate
+}
